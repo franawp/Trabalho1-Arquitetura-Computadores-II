1f9eff00dbb133a8bfabdc21ff3dc8c6437b5405
Francisco Oliveira <francisco55galo@icloud.com>
2023-06-09 17:18:56 -0300

    exec.h 2

---
+++ exec.h
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <instructionDecoder.h>
+using namespace std;
+
+/* 
+struct Flags {
+    bitset<8> opcode;
+    bitset<32> registradorA;
+    bitset<32> registradorB;
+    bitset<8> registradorC;
+    bool overflow;
+    bool neg;
+    bool zero;
+    bool carry;
+    bool memRead;
+    bool memWrite;
+};
+*/
+
+void execMemoria (Flags controle) {
+    
+}
